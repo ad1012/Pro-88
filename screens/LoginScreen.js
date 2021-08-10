7b6b2edbc9d5d2572ff1e87ceb33018bd9602b8f
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-08-09 23:42:43 -0500

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -14,6 +14,8 @@ export default class LoginScreen extends Component{
             address:'',
             confirmPassword:'',
             isModalVisible:'false'
+            itemValue:"", 
+            currencyCode:"",
         }
     }
     
