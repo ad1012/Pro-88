2c510573818a4a3ad5d48b943ecacd7b2c6ebc7b
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-08-16 10:11:55 -0500

    Update CustomSideBar.js

--- components/CustomSideBar.js
+++ components/CustomSideBar.js
@@ -3,13 +3,12 @@ import {StyleSheet, View, Text,TouchableOpacity, ImageBackground} from 'react-na
 import { DrawerItems} from 'react-navigation-drawer'
 import {Avatar} from 'react-native-elements';
 import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
 import firebase from 'firebase';
 import db from '../config'
 
 export default class CustomSideBarMenu extends Component{
   state = {
-      userId : firebase.auth().currentUser.email,
+     userId : firebase.auth().currentUser.email,
      image: null,
      name: "",
      docId : ""
@@ -22,8 +21,10 @@ export default class CustomSideBarMenu extends Component{
         aspect: [4, 3],
         quality: 1,
      });
-     console.log(uri);
-     if (!cancelled) {this. updateProfilePicture(uri);}
+     
+     if(!cancelled){
+     this.uploadImage(uri, this.state.userId); 
+     }
    }
 
   uploadImage = async (uri,imageName) => {
@@ -44,12 +45,6 @@ export default class CustomSideBarMenu extends Component{
      this.setState({image:"#"})
    })
   }
-
-   updateProfilePicture=(uri)=>{
-     db.collection('users').doc(this.state.docId).update({
-       image : uri
-     })
-   }
 
    getUserProfile(){
      db.collection('users')
